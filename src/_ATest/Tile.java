6b3dcd01b103a134d91b0dbddf2a6e05f69df438
Shannon Fisher <davine_chi@hotmail.com>
2019-10-02 15:28:19 -0500

    Provide layer support and Javadoc for Tile class

--- src/_ATest/Tile.java
+++ src/_ATest/Tile.java
@@ -1,9 +1,65 @@
 package _ATest;
 
+/****************************************************************************************************************
+ * This is a base class used for creation and management of various Tiles.
+ * 
+ * @author Shannon Fisher
+ */
 public class Tile extends Entity {
 
+	private int layer;
+	
+	/************************************************************************************************************
+	 * Constructor used to create a new Tile.
+	 * <p>
+	 * 
+	 * @param name
+	 *   The name of this Tile.
+	 */
 	public Tile(String name) {
 		
 		this.name = name;
+		this.layer = 0;
+	}
+	
+	/************************************************************************************************************
+	 * Constructor used to create a new Tile.
+	 * <p>
+	 * 
+	 * @param name
+	 *   The name of this Tile.
+	 * 
+	 * @param layer
+	 *   The layer ID for this Tile.
+	 */
+	public Tile(String name, int layer) {
+		
+		this.name = name;
+		this.layer = layer;
+	}
+	
+	/************************************************************************************************************
+	 * Accessor method to get this Tile's layer ID.
+	 * 
+	 * @return
+	 *   The layer ID for this Tile.
+	 */
+	public int getLayer() {
+		
+		return layer;
+	}
+	
+	/************************************************************************************************************
+	 * Modifier method to set the layer ID for this Tile.
+	 * 
+	 * @param layer
+	 *   The layer ID for this Tile.
+	 * 
+	 * @postcondition
+	 *   This Tile's layer ID has been modified.
+	 */
+	public void setLayer(int layer) {
+		
+		this.layer = layer;
 	}
 }
