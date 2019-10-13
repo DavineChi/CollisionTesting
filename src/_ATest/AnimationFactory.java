b67e1e37a438b394d945a45b74b50f9e46c21935
Shannon Fisher <davine_chi@hotmail.com>
2019-10-12 22:04:25 -0500

    Add vertical and idle Player Animation methods

--- src/_ATest/AnimationFactory.java
+++ src/_ATest/AnimationFactory.java
@@ -12,7 +12,7 @@ import org.newdawn.slick.SpriteSheet;
 public class AnimationFactory {
 	
 	/************************************************************************************************************
-	 * Factory method to create a looping animation from the specified SpriteSheet.
+	 * Factory method to create a looping animation from the specified SpriteSheet using horizontal offset.
 	 * <p>
 	 * 
 	 * @param spriteSheet
@@ -32,7 +32,7 @@ public class AnimationFactory {
 	 * 
 	 * @return The requested Animation.
 	 */
-	public static Animation createAnimation(SpriteSheet spriteSheet, int x, int y, int nSprites, int duration) {
+	public static Animation createAnimationHorizontal(SpriteSheet spriteSheet, int x, int y, int nSprites, int duration) {
 		
 		Image[] frames = new Image[nSprites + 1];
 		Animation result = null;
@@ -48,6 +48,86 @@ public class AnimationFactory {
 		}
 		
 		frames[3] = frames[1];
+		result = new Animation(frames, duration);
+		
+		result.setLooping(true);
+		
+		return result;
+	}
+	
+	/************************************************************************************************************
+	 * Factory method to create a looping animation from the specified SpriteSheet using vertical offset.
+	 * <p>
+	 * 
+	 * @param spriteSheet
+	 *   The spritesheet used for the requested Animation.
+	 * 
+	 * @param x
+	 *   The X-location of the starting sprite on the spritesheet (0-based indexing).
+	 * 
+	 * @param y
+	 *   The Y-location of the starting sprite on the spritesheet (0-based indexing).
+	 * 
+	 * @param nSprites
+	 *   The number of unique/distinct sprites for the Animation.
+	 * 
+	 * @param duration
+	 *   The duration to show each frame.
+	 * 
+	 * @return The requested Animation.
+	 */
+	public static Animation createAnimationVertical(SpriteSheet spriteSheet, int x, int y, int nSprites, int duration) {
+		
+		Image[] frames = new Image[nSprites + 1];
+		Animation result = null;
+		
+		for (int i = 0; i < (frames.length - 1); i++) {
+			
+			frames[i] = spriteSheet.getSprite(x, y).getScaledCopy(Constants.SPRITE_SCALE);
+			
+			y++;
+		}
+		
+		frames[3] = frames[1];
+		result = new Animation(frames, duration);
+		
+		result.setLooping(true);
+		
+		return result;
+	}
+	
+	/************************************************************************************************************
+	 * Factory method to create a looping idle Player animation from the specified SpriteSheet.
+	 * <p>
+	 * 
+	 * @param spriteSheet
+	 *   The spritesheet used for the requested Animation.
+	 * 
+	 * @param x
+	 *   The X-location of the starting sprite on the spritesheet (0-based indexing).
+	 * 
+	 * @param y
+	 *   The Y-location of the starting sprite on the spritesheet (0-based indexing).
+	 * 
+	 * @param duration
+	 *   The duration to show each frame.
+	 * 
+	 * @return The requested Animation.
+	 */
+	public static Animation createAnimationIdlePlayer(SpriteSheet spriteSheet, int x, int y, int duration) {
+		
+		int[] sequence = { 1, 2, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+		
+		Image[] frames = new Image[sequence.length];
+		Animation result = null;
+		
+		for (int k = 0; k < sequence.length; k++) {
+			
+			int yOffset = sequence[k];
+			
+			frames[k] = spriteSheet.getSprite(x, yOffset).getScaledCopy(Constants.SPRITE_SCALE);
+		}
+		
 		result = new Animation(frames, duration);
 		
 		result.setLooping(true);
