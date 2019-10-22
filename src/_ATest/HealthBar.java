4e580e9dd764963511054aec8174746c57f7a50d
Shannon Fisher <davine_chi@hotmail.com>
2019-10-22 13:45:14 -0500

    Update HealthBar implementation

--- src/_ATest/HealthBar.java
+++ src/_ATest/HealthBar.java
@@ -1,12 +1,22 @@
 package _ATest;
 
+import org.lwjgl.util.Timer;
 import org.newdawn.slick.geom.Rectangle;
 
 public class HealthBar extends CommonBar {
 	
+	public static enum State { EMPTY, FULL, COMBAT, REGEN };
+	
+	private HealthBar.State state;
+	
+	private Timer timer;
+	
 	public HealthBar(float xPosition, float yPosition, float width, float height) {
 		
 		super(xPosition, yPosition, width, height);
+		
+		state = State.FULL;
+		timer = new Timer();
 	}
 	
 	@Override
@@ -23,10 +33,40 @@ public class HealthBar extends CommonBar {
 		return fillBar;
 	}
 	
+	private void queryFillLevel() {
+		
+		if ((float)(Player.instance().getHitPoints()) < (float)(Player.instance().getMaxHitPoints())) {
+			
+			state = State.REGEN;
+		}
+	}
+	
+	public HealthBar.State getState() {
+		
+		return state;
+	}
+	
+	public void setState(HealthBar.State state) {
+		
+		this.state = state;
+	}
+	
 	public void update() {
 		
-		float fillFactor = (float)(Player.instance().getHitPoints()) / (float)(Player.instance().getMaxHitPoints());
+		queryFillLevel();
 		
-		fillBar.setWidth(width * fillFactor);
+		if (state == State.REGEN) {
+			
+			Timer.tick();
+			
+			if (timer.getTime() / 1000 > 3) {
+				
+				timer.reset();
+				
+				float fillFactor = (float)(Player.instance().getHitPoints()) / (float)(Player.instance().getMaxHitPoints());
+				
+				fillBar.setWidth(width * fillFactor);
+			}
+		}
 	}
 }
