5085b241348c5f88cf34555154cc03930f585da9
Shannon Fisher <davine_chi@hotmail.com>
2019-10-27 21:10:14 -0500

    Add Javadoc

--- src/_ATest/Currency.java
+++ src/_ATest/Currency.java
@@ -4,16 +4,58 @@ public class Currency {
 	
 	private int value;
 	
+	/************************************************************************************************************
+	 * Constructor to create a new Currency object.
+	 * <p>
+	 * 
+	 */
 	public Currency() {
 		
 		this.value = 0;
 	}
 	
+	/************************************************************************************************************
+	 * Modifier method to add to this Currency object's value.
+	 * 
+	 * @param value
+	 *   the amount of value to add
+	 * 
+	 * @postcondition
+	 *   This Currency object's value has been modified.
+	 */
 	public void addValue(int value) {
 		
 		this.value = this.value + value;
 	}
 	
+	/************************************************************************************************************
+	 * Modifier method to remove from this Currency object's value.
+	 * 
+	 * @param value
+	 *   the amount of value to remove
+	 * 
+	 * @postcondition
+	 *   This Currency object's value has been modified.
+	 * </br>
+	 * </br>
+     * <b>Note:</b>
+     *   This method prevents negative-resulting values from being set.
+	 */
+	public void removeValue(int value) {
+		
+		if (this.value - value >= 0) {
+			
+			this.value = this.value - value;
+		}
+	}
+	
+	/************************************************************************************************************
+	 * Accessor method to retrieve the value of this Currency object.
+	 * <p>
+	 * 
+	 * @return
+	 *   The value of this Currency object.
+	 */
 	public int getValue() {
 		
 		return value;
@@ -66,6 +108,6 @@ public class Currency {
 			gold = strVal.substring(0, strVal.length() - 4);
 		}
 		
-		return gold + " G | " + silver + " S | " + copper + " C";
+		return gold + " G    " + silver + " S    " + copper + " C";
 	}
 }
